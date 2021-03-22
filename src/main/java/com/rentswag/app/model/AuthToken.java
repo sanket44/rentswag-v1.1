b3f337ccdd054d4d1b4a1bce6ca301f2173e2415
Sanket Dhande <54401203+sanket44@users.noreply.github.com>
2021-03-22 23:18:47 +0530

    Update AuthToken.java
    bug fixing about controller

--- src/main/java/com/rentswag/app/model/AuthToken.java
+++ src/main/java/com/rentswag/app/model/AuthToken.java
@@ -3,6 +3,20 @@ package com.rentswag.app.model;
 public class AuthToken {
 
     private String token;
+      private int roles;
+
+	public AuthToken(String token, int roles) {
+		this.token = token;
+		this.roles = roles;
+	}
+
+	public int getRoles() {
+		return roles;
+	}
+
+	public void setRoles(int roles) {
+		this.roles = roles;
+	}
 
     public AuthToken(){
 
